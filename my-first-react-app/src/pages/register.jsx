7c6aa64d5cb6cd0fb68436634cd276e9e2f6f2ad
kirankumae-Matham96 <mathamkirankumar96@gamil.com>
2021-06-26 01:15:06 +0530

    fix: add styling to register page

--- my-first-react-app/src/pages/register.jsx
+++ my-first-react-app/src/pages/register.jsx
@@ -57,25 +57,34 @@ class Register extends Component{
     const isEmail = name === 'email';
     const isPassword = name === 'password';
     const isConfirmPassword = name === 'confirmPassword'
-    const nameTest = /^[A-Z]{1}[A-Za-z]{2,25}$/i;
+    const nameTest = /^[A-Z]{1}[A-Za-z]{2,25}$/;
     const emailTest = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
 
+
     validity[name] = value.length > 0;
-    fieldValidationErrors[name] = validity[name] ? '' : `${name} is required and cannot be empty!`;
+    fieldValidationErrors[name] = validity[name] ? '' : `${name} is required!`;
 
     if (validity[name]) {
       if (isFirstName) {
         validity[name] = nameTest.test(value);
-        fieldValidationErrors[name] = validity[name] ? '' : `${name} should be a valid one!`;
+        fieldValidationErrors[name] = validity[name] ? '' : `${name} is invalid!`;
       }
       if (isLastName) {
         validity[name] = nameTest.test(value);
-        fieldValidationErrors[name] = validity[name] ? '' : `${name} should be a valid one!`;
+        fieldValidationErrors[name] = validity[name] ? '' : `${name} is invalid!`;
       }
       if (isEmail) {
         validity[name] = emailTest.test(value);
-        fieldValidationErrors[name] = validity[name] ? '' : `${name} should be a valid one!`;
       }
+      if (isPassword) {
+        var pwd = value;
+        validity[name] = value.length >= 8;
+        fieldValidationErrors[name] = validity[name] ? '' : `${name} should at least be 8 chars!`;
+      }
+      // if (isConfirmPassword) {
+      //   validity[name] = (value === pwd);
+      //   fieldValidationErrors[name] = validity[name] ? '' : `${name} should match!`;
+      // }
     }
 
     this.setState({
@@ -106,38 +115,72 @@ class Register extends Component{
 
   render() {
     const { formValues, formErrors, isSubmitting } = this.state
-    return(
-      <Formik>
-        <Form onSubmit={this.handleSubmit} >
-          <div>{ <Title />}</div>
-          <div>
-            <input name='firstName' type='text' placeholder='first name' onChange={this.handleChange} value={ formValues.firstName}/>
-            <div className='error-message'>{ formErrors.firstName}</div>
-            <input name='lastName' type='text' placeholder='last name' onChange={ this.handleChange } />
-            <div className='error-message'>{ formErrors.lastName}</div>
-          </div>
-          <input name='email' type='email' placeholder='email' onChange={ this.handleChange } />
-            <div className='error-message'>{ formErrors.email}</div>
-            <p>You can use letters, numbers & periods</p>
-          <div>
-            <input name='password' type='password' placeholder='password' onChange={ this.handleChange } />
-            <div className='error-message'>{ formErrors.password}</div>
-            <input name='confirmPassword' type='password' placeholder='confirm password' onChange={ this.handleChange } />
-            <div className='error-message'>{ formErrors.confirmPassword}</div>
-          </div>
-          <p>Use 8 or more characters with a mix of letters, numbers & symbols</p>
-          <div>
-            <input type='checkbox' ></input>
-            <label htmlFor="">show password</label>
-          </div>
-          <div>
-          <Link to='/'>
-            <button className='button'>Home</button>
-          </Link>
-            <button className='button' type='submit' disabled={isSubmitting} >Register</button>
-          </div>
-        </Form>
-      </Formik>
+    return (
+      <div className='register-page'>
+        <Formik>
+          <Form onSubmit={this.handleSubmit} >
+            <div className='title'>{<Title />}</div>
+              <div className='text name'>
+                <div className='parallel-fields left-field' >
+                  <label className='custom-field'>
+                    <input className='firstName' name='firstName' type='text' onChange={this.handleChange} value={ formValues.firstName} required/>
+                    <span className='placeHolder'>First Name</span>
+                  </label>
+                  <div className='error-message'>{ formErrors.firstName}</div>
+                </div>
+              <div className='parallel-fields right-field' >
+              <label className='custom-field'>
+                  <input className='lastName' name='lastName' type='text' onChange={ this.handleChange } required />
+                  <span className='placeHolder'>Last Name</span>
+              </label>
+                  <div className='error-message'>{ formErrors.lastName}</div>
+                </div>
+              </div>
+            <div className='email-div'>
+              <label className='custom-field'>
+                <input className='email text' name='email' type='email' onChange={ this.handleChange } required />
+                <span className='placeHolder'>Email</span>
+              </label>
+            </div>
+              <div className='error-message'>{formErrors.email}</div>
+              <p>You can use letters, numbers & periods</p>
+              <div className='text pwd-section'>
+              <div className='parallel-fields left-field' >
+              <label className='custom-field'>
+                  <input className='password' id='password' name='password' type='password' onChange={ this.handleChange } required />
+                  <span className='placeHolder'>Password</span>
+              </label>
+                  <div className='error-message'>{ formErrors.password}</div>
+                </div>
+              <div className='parallel-fields right-field' >
+              <label className='custom-field'>
+                  <input className='password' id='password' name='confirmPassword' type='password' onChange={ this.handleChange } required />
+                  <span className='placeHolder'>Confirm Password</span>
+              </label>
+                  <div className='error-message'>{ formErrors.confirmPassword}</div>
+                </div>
+              </div>
+              <p>Use 8 or more characters with a mix of letters, numbers & symbols</p>
+              <div className='checkbox-content'>
+                <input type='checkbox' ></input>
+                <label>show password</label>
+              </div>
+            <footer className='foot'>
+              <div className='signin-link'>
+                <Link to='/login'>
+                  <a>signin instead</a>
+                </Link>
+              </div>
+              <div className='button'>
+                <button className='register-btn' type='submit' disabled={isSubmitting} >Register</button>
+                <Link to='/'>
+                  <button className='home-btn'>Home</button>
+                </Link>
+              </div>
+            </footer>
+          </Form>
+        </Formik>
+      </div>
     )
   }
 }
