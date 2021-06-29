5727dd2754deb79b9eae5480022987ef80b7da2e
kirankumae-Matham96 <mathamkirankumar96@gamil.com>
2021-06-29 14:12:01 +0530

    feat: able to fetch data from fundoonotes backend api

--- my-first-react-app/src/pages/home.jsx
+++ my-first-react-app/src/pages/home.jsx
@@ -1,5 +1,6 @@
 // import React, { Component} from 'react';
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 // import axios from '../components/axios';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
@@ -39,23 +40,46 @@ const Home = () =>
   const [last, setLast] = useState([]);
   const [pic, setPic] = useState([]);
 
-  const getData = async () => {
+  const getDataFromOnlineSite = async () => {
     await axios.get('https://api.randomuser.me/').then((res) =>
     {
-      console.log(res.data);
+      console.log(res.data.results)
       setFirst(res.data.results[0].name.first);
       setLast(res.data.results[0].name.last);
       setPic(res.data.results[0].picture.large);
-      console.log('user: ',first);
     })
+  }
+
+  const getAllUsers = async () => {
+    await axios.get('http://fundoonotes.incubation.bridgelabz.com/api/user')
+      .then((res) => {
+        console.log('res.data: ', res.data);
+        console.log('res.data[0]: ',res.data[0]);
+        setFirst(res.data[0].firstName);
+        setLast(res.data[0].lastName);
+    })
+  }
+
+  const getUserById = async () => {
+    await axios.get('http://fundoonotes.incubation.bridgelabz.com/api/user/5bbda7df60eb5e004087da75')
+      .then(res => {
+        console.log(`user by id: ${ res.data.firstName } -> ${ res.data.email }`);
+        setFirst(res.data.firstName);
+        setLast(res.data.lastName);
+      })
   }
 
   return (
     <div>
       <h1>Using functional component</h1>
-      {<div>{`First Name: ${ first }`} <br/> {`Last Name: ${last}`}</div> }
+      <ol>
+        {/* {`dataFrom swagger: ${users.firstName}`} */}
+      </ol>
+      <div>{`First Name: ${ first }`} <br/> {`Last Name: ${last}`}</div>
       <img src={pic} alt='pic' />
-      <button onClick={getData}>get data</button>
+      <button onClick={getDataFromOnlineSite}>get data from online site</button>
+      <button onClick={getAllUsers}>get all users from swagger</button>
+      <button onClick={getUserById}>get user by id from swagger</button>
       <Link to='/register'>
         <button>Register</button>
       </Link>
@@ -71,26 +95,31 @@ const Home = () =>
  */
 // const api = axios.create({
 //   // baseURL: 'http://localhost:3000/'
-//   // baseURL: 'http://fundoonotes.incubation.bridgelabz.com/explorer/#/'
-//   baseURL: 'https://api.randomuser.me/'
+//   baseURL: 'http://fundoonotes.incubation.bridgelabz.com/api/'
+//   // baseURL: 'https://api.randomuser.me/'
 //   // baseURL: 'http://localhost:3001'
 // })
 
 // class Home extends Component
 // {
-//   constructor(){
-//   super();
+//   constructor()
+//   {
+//     super();
 
-//   this.state = {
-//     firstName: '',
-//     lastName: '',
-//     picture:''
+//     this.state = {
+//       firstName: '',
+//       lastName: '',
+//       email: ''
 //     }
 
-//     // api.get('/').then(res =>
-//     api.get('/users/').then(res =>
+//     api.get('/user/').then(res =>
 //     {
-//       this.setState({ firstName: res.data.results[0].name.first, lastName: res.data.results[0].name.last, picture: res.data.results[0].picture.large });
+//       console.log(res);
+//       this.setState({ firstName: res.data.firstName, lastName: res.data.lastName, email: res.data.email });
+//     }).catch((err) => { console.log('Error: ', err) });
+
+//     api.get('/user/:id').then(res => {
+//       this.setState({ firstName: res.data.firstName, lastName: res.data.lastName, email: res.data.email });
 //     }).catch((err) => { console.log('Error: ', err) });
 //   }
 
@@ -100,11 +129,12 @@ const Home = () =>
 //         <h1>Using class component</h1>
 //         <li>{this.state.firstName}</li>
 //         <li>{this.state.lastName}</li>
-//         <img src={this.state.picture} />
+//         <Link to='/register'>
+//           <button>Register</button>
+//         </Link>
 //       </div>
 //     )
 //   }
 // }
-
 
 export default Home;
