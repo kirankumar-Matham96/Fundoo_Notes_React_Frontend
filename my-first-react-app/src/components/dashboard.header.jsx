ebd8af149da8688f550d155aecdd410b5693ebef
kirankumae-Matham96 <mathamkirankumar96@gamil.com>
2021-07-02 06:36:20 +0530

    feat: add icons for sidenav in dashboard

--- my-first-react-app/src/components/dashboard.header.jsx
+++ my-first-react-app/src/components/dashboard.header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Title from './title';
-import { FaStickyNote, FaBars, FaRedo, FaCog, FaEquals, FaSearch } from 'react-icons/fa';
+import { FaStickyNote, FaBars, FaRedo, FaCog, FaEquals, FaSearch, FaRegStickyNote, FaRegBell, FaPencilAlt, FaRegFileArchive, FaRegTrashAlt } from 'react-icons/fa';
 import { Row, Col, Form, FormControl, Container } from 'react-bootstrap';
 import '../scss/dashboard.header.scss'
 
@@ -13,7 +13,7 @@ const header = () =>
             <Col>
               <div className='left'>
                 <FaBars className='icon'/>
-                <FaStickyNote className='icon notes'/>
+                <FaStickyNote className='icon solid-notes'/>
                 <h4 className='custom-title'><Title /></h4>
                 <Form.Group className='middle'>
                   <FaSearch className='search-icon icon'/>
@@ -27,7 +27,26 @@ const header = () =>
               </div>
             </Col>
           </Row>
+      </div>
+      <div className='body'>
+        <div className="side-nav">
+          <div className="notes">
+            <FaRegStickyNote className='icon' />
+          </div>
+          <div className="remainders">
+            <FaRegBell className='icon' />
+          </div>
+          <div className="edit-notes">
+            <FaPencilAlt className='icon' />
+          </div>
+          <div className="archive">
+            <FaRegFileArchive className='icon' />
+          </div>
+          <div className="trash">
+            <FaRegTrashAlt className='icon' />
+          </div>
         </div>
+      </div>
       </div>
 
   )
