03cfedf7edb6ea0bf175f0025e89df53420f5489
kirankumae-Matham96 <mathamkirankumar96@gamil.com>
2021-07-09 01:53:26 +0530

    perf: upgrade side-nav style

--- my-first-react-app/src/components/sideNav.jsx
+++ my-first-react-app/src/components/sideNav.jsx
@@ -11,31 +11,31 @@ const sideNav = ({show}) =>
 
         <ul>
           <li>
-            <div className='inline-content'>
+            <div className='inline-content1 active'>
               <FaRegStickyNote className='icon side-icon' />
               <a className='linkTag'>Notes</a>
             </div>
           </li>
           <li>
-            <div className='inline-content'>
+            <div className='inline-content2'>
               <FaRegBell className='icon side-icon' />
               <a className='linkTag'>Remainders</a>
             </div>
           </li>
           <li>
-            <div className='inline-content'>
+            <div className='inline-content2'>
               <FaPencilAlt className='icon side-icon' />
               <a className='linkTag'>Edit</a>
             </div>
           </li>
           <li>
-            <div className='inline-content'>
+            <div className='inline-content2'>
               <FaRegFileArchive className='icon side-icon' />
               <a className='linkTag'>Archive</a>
             </div>
           </li>
           <li>
-            <div className='inline-content'>
+            <div className='inline-content2'>
               <FaRegTrashAlt className='icon side-icon' />
               <a className='linkTag'>Trash</a>
             </div>
