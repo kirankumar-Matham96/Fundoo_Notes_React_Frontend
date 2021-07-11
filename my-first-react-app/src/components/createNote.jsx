364863f7101a98754e8cc7bd382a9adb675db0f4
kirankumae-Matham96 <mathamkirankumar96@gamil.com>
2021-07-11 16:18:20 +0530

    feat: add take a note feature to dashboard

--- my-first-react-app/src/components/createNote.jsx
+++ my-first-react-app/src/components/createNote.jsx
@@ -1,11 +1,21 @@
-const takeANote = () =>
+import "../scss/createNotes.scss";
+import { Button } from 'react-bootstrap';
+import {RiPushpin2Line} from 'react-icons/ri';
+import {FaRegPlusSquare} from 'react-icons/fa';
+const createNote = () =>
 {
-  return (
-    <div className='
-    '>
-      Hello
+  return (<div className='createNote'>
+    <div className='main_note'>
+      <div className='row1'>
+        <input type='text' placeholder='Title' />
+        <textarea placeholder='Take a note...'></textarea>
+      </div>
+      <div className='row2'>
+        <RiPushpin2Line className='pin'/>
+        <Button className='cancel'>cancel</Button>
+      </div>
     </div>
-  )
+  </div>)
 }
 
-export default takeANote;
+export default createNote;
--- my-first-react-app/src/services/user.js
+++ my-first-react-app/src/services/user.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
+// import { BASE_URL } from 'dotenv';
 require('dotenv').config();
-import { BASE_URL } from 'dotenv';
 
 class Services
 {
