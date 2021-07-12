c45f6e4b2b2633bf9f42fd5babbca6b1fb7f9f48
kirankumae-Matham96 <mathamkirankumar96@gamil.com>
2021-07-12 22:37:28 +0530

    style: change style for dashboard

--- my-first-react-app/src/components/note.jsx
+++ my-first-react-app/src/components/note.jsx
@@ -4,6 +4,7 @@ import { Card } from 'react-bootstrap';
 import { RiInboxArchiveLine} from 'react-icons/ri';
 import {BiBellPlus, BiUserPlus, BiImage} from 'react-icons/bi';
 import { CgTrash } from 'react-icons/cg';
+import { IoColorPaletteOutline } from 'react-icons/io5';
 
 const note = (props) =>
 {
@@ -16,14 +17,17 @@ const note = (props) =>
 
     <Card style={{ width: '13rem' }} className='note'>
   <Card.Body>
-        <Card.Title>{ props.title }</Card.Title>
-        <Card.Text>
-          { props.content}
-        </Card.Text>
+        <div className="note_head">
+          <Card.Title>{ props.title }</Card.Title>
+          <Card.Text>
+            { props.content}
+          </Card.Text>
+        </div>
         <div className='note_icons'>
           <BiBellPlus className='inner_note_icons'/>
           <BiUserPlus className='inner_note_icons'/>
           <BiImage className='inner_note_icons'/>
+          <IoColorPaletteOutline className='inner_note_icons' />
           <RiInboxArchiveLine className='inner_note_icons' />
           <CgTrash className='inner_note_icons' onClick={deleteNote}/>
         </div>
