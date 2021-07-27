bbc63b2c06a13b5f3f834766faf962d5656f413d
kirankumae-Matham96 <mathamkirankumar96@gamil.com>
2021-07-28 01:20:51 +0530

    refactor: modified jenkins script file again

--- my-first-react-app/jenkinsFile.sh
+++ my-first-react-app/jenkinsFile.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-cd /home/ubuntu/my-first-react-app/
-npm i
+# cd /home/ubuntu/my-first-react-app/
+# npm i
+/home/ubuntu/my-first-react-app/ npm i
 echo "installing node modules..."
-npm build
+# npm build
+/home/ubuntu/my-first-react-app/npm build
 echo "optimizing build for deployment..."
-npm start
+# npm start
+/home/ubuntu/my-first-react-app/ npm start
 echo "server starting..."
