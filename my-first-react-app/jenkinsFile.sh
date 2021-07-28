c254baeb9976751df3967b61fcc767fd11a73f54
kirankumae-Matham96 <mathamkirankumar96@gamil.com>
2021-07-28 15:22:22 +0530

    refactor: modify script file

--- my-first-react-app/jenkinsFile.sh
+++ my-first-react-app/jenkinsFile.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -x
 
 cd /home/ubuntu/my-first-react-app/
-ls -l
+present_working_directory=$(pwd)
+echo "present working directory is: $present_working_directory"
 echo "installing node modules..."
 npm i
 echo "server starting..."
 npm run start
+echo "server is running at port 3000"
