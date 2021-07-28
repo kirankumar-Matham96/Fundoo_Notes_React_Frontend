8aba394b789267e64e3afdcd1dc65a5e7fe81f30
kirankumae-Matham96 <mathamkirankumar96@gamil.com>
2021-07-28 14:05:54 +0530

    refactor: change npm run start to npm start

--- my-first-react-app/jenkinsFile.sh
+++ my-first-react-app/jenkinsFile.sh
@@ -1,12 +1,6 @@
 #!/bin/bash -x
 
-# cd /home/ubuntu/my-first-react-app/
-#/home/ubuntu/my-first-react-app/ npm i
-npm i
 echo "installing node modules..."
-#/home/ubuntu/my-first-react-app/npm build
-#npm run build
-echo "optimizing build for deployment..."
-#/home/ubuntu/my-first-react-app/ npm start
-npm run start
+npm i
 echo "server starting..."
+npm start
